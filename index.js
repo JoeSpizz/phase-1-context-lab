3e15528bca3e68b1f5c1ed677d71b2a3601c6948
Joe <spizjo01@gmail.com>
2022-07-10 18:14:17 -0600

    commit tree 58e12dae0dbe6640fca25366851ece5fb6c48edd

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+    let [firstName, familyName, title, payPerHour] = array;
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+function createEmployeeRecords(arrayNest){
+   return arrayNest.map(e => createEmployeeRecord(e))
+}
 
+function createTimeInEvent(punch){
+    let [date, hour] = punch.split(" ")
+    let eventObj = {
+        type: "TimeIn",
+        hour: parseInt(hour,10),
+        date
+    }
+    this.timeInEvents.push(eventObj)
+   return this
+    
+}
+
+function createTimeOutEvent(punch){
+    let [date, hour] = punch.split(" ")
+    let eventObj = {
+        type: "TimeOut",
+        hour: parseInt(hour,10),
+        date
+    }
+    this.timeOutEvents.push(eventObj)
+   return this
+}
+
+function hoursWorkedOnDate (date) {
+   const timeIn = this.timeInEvents.find(e => e.date===date)
+   const timeOut = this.timeOutEvents.find(e=>e.date===date)
+  
+   return (timeOut.hour-timeIn.hour)/100
+}
+
+function wagesEarnedOnDate (date){
+    const hoursWorked = hoursWorkedOnDate.call(this, date)
+    return this.payPerHour * hoursWorked 
+   }
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +69,12 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(array, name){
+    return array.find(emp=> emp.firstName === name)
+
+}
+
+function calculatePayroll(arrayOfEmps){
+    let sumOfPay = arrayOfEmps.map(e=> allWagesFor.call(e)).reduce((a,b)=> a+b, 0)
+        return sumOfPay
+    }
